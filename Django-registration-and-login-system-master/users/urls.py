e71eee7950b85a549d6e7b9c96d24ffa1695fa8b
Abhigyan Kumar <abhigkumar709@gmail.com>
2022-12-08 21:30:02 +0530

    Errors Commit

--- Django-registration-and-login-system-master/users/urls.py
+++ Django-registration-and-login-system-master/users/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .views import home, profile, RegisterView
+from . import views
 
 urlpatterns = [
     path('', home, name='users-home'),
+    path('add', views.addition, name='add'),
+    path('sub', views.subtraction, name='sub'),
+    path('multi', views.multiplication, name='multi'),
+    path('div', views.division, name='div'),
     path('register/', RegisterView.as_view(), name='users-register'),
     path('profile/', profile, name='users-profile'),
+    
 ]
--- Django-registration-and-login-system-master/users/views.py
+++ Django-registration-and-login-system-master/users/views.py
@@ -13,6 +13,74 @@ def home(request):
     return render(request, 'users/home.html')
 
 
+def addition(request):
+
+    num1 = request.POST['num1']
+    num2 = request.POST['num2']
+
+    if num1.isdigit() and num2.isdigit():
+        a = int(num1)
+        b = int(num2)
+        res = a + b
+
+        return render(request, "result.html", {"result": res})
+    else:
+        res = "Only digits are allowed"
+        return render(request, "result.html", {"result": res})
+
+def subtraction(request):
+
+    num1 = request.POST['num1']
+    num2 = request.POST['num2']
+
+    if num1.isdigit() and num2.isdigit():
+        a = int(num1)
+        b = int(num2)
+        res = a - b
+
+        return render(request, "result.html", {"result": res})
+    else:
+        res = "Only digits are allowed"
+        return render(request, "result.html", {"result": res})
+
+
+def multiplication(request):
+
+    num1 = request.POST['num1']
+    num2 = request.POST['num2']
+
+    if num1.isdigit() and num2.isdigit():
+        a = int(num1)
+        b = int(num2)
+        res = a * b
+
+        return render(request, "result.html", {"result": res})
+    else:
+        res = "Only digits are allowed"
+        return render(request, "result.html", {"result": res})
+
+
+def division(request):
+
+    num1 = request.POST['num1']
+    num2 = request.POST['num2']
+
+    
+    if num1.isdigit() and num2.isdigit():
+        a = int(num1)
+        b = int(num2)
+
+        if b == 0:
+            res = "Zero divide error"
+            return render(request, "result.html", {"result": res})
+        else:
+            res = a / b
+            return render(request, "result.html", {"result": res})
+    else:
+        res = "Only digits are allowed"
+        return render(request, "result.html", {"result": res})
+ 
+
 class RegisterView(View):
     form_class = RegisterForm
     initial = {'key': 'value'}
